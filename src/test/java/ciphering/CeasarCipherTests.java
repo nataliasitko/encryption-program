5a26690b4e6a3f2123d2ee243ee2aeced2f19b9e
Natalia Sitko~ <nataliasitko@interia.pl>
2022-04-15 16:13:24 +0200

    Update CaesarCipherTests

--- src/test/java/ciphering/CeasarCipherTests.java
+++ src/test/java/ciphering/CeasarCipherTests.java
@@ -1,6 +1,5 @@
 package ciphering;
 
-import ciphering.CaesarCipher;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
